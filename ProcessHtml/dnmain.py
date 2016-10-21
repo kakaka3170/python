e303d15030ab31c8bd002a27841b8262381c87bb
yangjinning <ydnss3170@gmail.com>
2016-10-21 22:09:34 +0800

    add html process code

---
+++ ProcessHtml/dnmain.py
@@ -0,0 +1,19 @@
+
+
+
+
+from html.Parset import HTMLParser;
+ 
+ 
+ 
+class MyParser(HTMLParser):
+    def __init__ (self):
+        pass;
+    def handle_starttag (self,tag,attrs):
+        pass;
+
+print ('===================html process===================')
+
+
+pass;
+
