235c8045dee4b811cc168bf0ca7bb389f9649d83
yangjinning <ydnss3170@gmail.com>
2016-10-21 22:27:48 +0800

    html can import library correctly

--- ProcessHtml/dnmain.py
+++ ProcessHtml/dnmain.py
@@ -2,17 +2,25 @@
 
 
 
-from html.Parset import HTMLParser;
+from html.parser import HTMLParser;
  
  
  
 class MyParser(HTMLParser):
-    def __init__ (self):
-        pass;
     def handle_starttag (self,tag,attrs):
+        print('<%s>'%tag);
+        pass;
+    def handle_endtag (self,tag):
+        print('</%s>'%tag);
         pass;
 
 print ('===================html process===================')
+
+
+YdnParser = MyParser();
+fd = open('ProcessHtml/Ydn.html','r');
+content = fd.read();
+YdnParser.feed(content);
 
 
 pass;
