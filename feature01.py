90b42e85f2ac7786aaca81409212c3465196880b
Jinning Yang <ydnss3170@163.com>
2016-10-21 16:06:54 +0800

    chengfa koujuebiao  dao 11

--- feature01.py
+++ feature01.py
@@ -1,8 +1,8 @@
 
 
-for i in range(1,10):
+for i in range(1,12):
     for j in range(1,i+1):
-        print ('%2d X%2d =%2d'%(j,i,i*j),end=' ');
+        print ('%2d X%2d = %2d'%(j,i,i*j),end=' ');
     print('');
 
 
